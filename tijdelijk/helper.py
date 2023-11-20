0bc8f0f5a4238aa17c8d32d2b19d4401858c77a2
D.Wimmers <daanrw@hotmail.com>
2023-11-20 15:26:54 +0100

    functies toegevoegd

--- tijdelijk/helper.py
+++ tijdelijk/helper.py
@@ -1,11 +1,10 @@
-'''(tekst="header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
-print()'''
-
+def decoreer(tekst=""):
+    lengte = len(tekst) + 4
+    print()
+    print(lengte * "*")
+    print(f"* {tekst} *")
+    print(lengte * "*")
+    print()
 
 '''
 mijn_string=("waterijsje")
---
+++ tijdelijk/tijdelijk.py
@@ -0,0 +1,28 @@
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen ={
+        "aardbei":3,
+        "vanille":4,
+        "chocolade":5
+    } 
+    huidige_prijs= prijzen["aardbei"]
+    aanbieding1=huidige_prijs*0.8
+    reclame_tekst=f"vandaag in de aabieding: vanille-ijs, 1 liter - slechts € {aanbieding1}"
+
+    reclame_tekst2=reclame_tekst[:62]
+
+    reclame_tekst3=reclame_tekst2.upper()
+
+    reclame_tekst4=reclame_tekst3.split()
+
+
+    for el in reclame_tekst4:
+        if len(el) < 4:
+            print(el.lower())
+        else:
+            print(el.upper())
+            
+decoreer("Aanbieding")
+print_aanbieding()
+decoreer("Aanbieding")
