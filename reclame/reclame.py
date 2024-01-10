18554e5255cb81966a27192d83c81755db4a60d5
D.Wimmers <daanrw@hotmail.com>
2024-01-10 10:13:09 +0100

    huiswerk les 8 af

--- reclame/reclame.py
+++ reclame/reclame.py
@@ -1,3 +1,7 @@
+
+
+
+
 aanbieding_1={
 "smaak":"aardbei",
 "prijs":4 , 
@@ -23,3 +27,38 @@ laag_hoog={
 "mijn_lijst":[220, 430, 125, 160, 205, 90, 345]
 }
 
+maxi=max(laag_hoog["mijn_lijst"])
+mini=min(laag_hoog["mijn_lijst"])
+
+print(str(maxi)+","+str(mini))
+
+mean=sum(laag_hoog["mijn_lijst"])/len(laag_hoog["mijn_lijst"])
+print(mean)
+
+print(f"De gemiddelde inkomsten deze week zijn {int(mean)} euro.")
+
+
+mijn_functie_2={
+12.3:[15,9,36,4],
+12.2:[14, 10, 24, 6],
+10.5:[15, 5, 50, 2],
+100.20:[120, 80, 2000, 5]
+}
+
+
+meervoudig={
+"invoer_lijst":[10,5,3,2,1,2,9]
+}
+
+def laag_hoog(mijn_lijst):
+    return max("mijn_lijst"), min("mijn_lijst")
+
+def meervoudig(invoer_lijst):
+    return laag_hoog("mijn_lijst")
+
+
+def combinatie(invoer_lijst_2):
+    korte_lijst = meervoudig("invoer_lijst")
+    return mijn_functie_2(korte_lijst)
+
+print(combinatie(mijn_functie_2))
--- tijdelijk/helper.py
+++ tijdelijk/helper.py
@@ -26,3 +26,18 @@ print("buiten: ",b)
 print("buiten: ",b)'''
 
 
+'''def fooi_pp(bedrag, persoon):
+    bedrag_pp=bedrag/persoon
+    return f"het bedrag per persoon is {bedrag_pp} euro"
+
+b = int(input("Welk bedrag zit er in de fooienpot? "))
+p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
+
+print(fooi_pp(b,p))'''
+
+mijn_functie_2={
+12.3:[15,9,36,4],
+12.2:[14, 10, 24, 6],
+10.5:[15, 5, 50, 2],
+100.20:[120, 80, 2000, 5]
+}
